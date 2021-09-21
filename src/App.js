d34018741e85a310f3e5342298c17acc7b17a221
Pratheesh P S <90699696+pratheeshps@users.noreply.github.com>
2021-09-21 18:40:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,106 @@
-import React, { Children, useState } from "react";
+import React, { Children, useContext, useEffect, useState } from "react";
 import "./styles.css";
 
-function Wizard({ children }) {
-  const [activePage, setActivePage] = useState(0);
-  const pages = Children.toArray(children);
-  const currentPage = pages[activePage];
-
-  const handlePrevAction = () => {
-    setActivePage((index) => index - 1);
-  };
-
-  const handleNextAction = () => {
-    setActivePage((index) => index + 1);
-  };
-
-  return (
-    <div className="wizard-container">
-      <div className="wizard-content">{currentPage}</div>
-      <div className="wizard-actions">
-        <button
-          type="button"
-          className="wizard__buttons-left"
-          disabled={activePage <= 0}
-          onClick={handlePrevAction}
-        >
-          Previous
-        </button>
-        <button
-          type="button"
-          className="wizard__buttons-right"
-          disabled={activePage === pages.length - 1}
-          onClick={handleNextAction}
-        >
-          Next
-        </button>
-      </div>
-    </div>
-  );
-}
+const WizardContext = React.createContext();
+const useWizardContext = () => {
+  const context = useContext(WizardContext);
+  if (!context) {
+    throw new Error(
+      "The compound component cannot be rendered outside parent component"
+    );
+  }
+  return context;
+};
 
 const Page1 = () => <h2>Page 1 </h2>;
 const Page2 = () => <h2>Page 2 </h2>;
 const Page3 = () => <h2>Page 3 </h2>;
 
+const ButtonPrev = ({ children, as: Comp = "button", ...props }) => {
+  const { activePageIndex, goToPrev } = useWizardContext(WizardContext);
+
+  return (
+    <Comp disabled={activePageIndex <= 0} onClick={goToPrev} {...props}>
+      {children}
+    </Comp>
+  );
+};
+
+const ButtonNext = ({ children, as: Comp = "button", ...props }) => {
+  const { activePageIndex, goToNext, steps } = useWizardContext(WizardContext);
+
+  return (
+    <Comp
+      {...props}
+      disabled={activePageIndex === steps - 1}
+      onClick={goToNext}
+    >
+      {children}
+    </Comp>
+  );
+};
+
+function Pages({ children }) {
+  const { activePageIndex, setSteps } = useWizardContext(WizardContext);
+  const pages = Children.toArray(children);
+  const steps = Children.count(children);
+  useEffect(() => {
+    console.log(steps);
+    setSteps(steps);
+  }, [steps, setSteps]);
+  const currentPage = pages[activePageIndex];
+  return <div className="wizard-content">{currentPage}</div>;
+}
+
+function Wizard({ children }) {
+  const [activePageIndex, setActivePageIndex] = useState(0);
+  const [steps, setSteps] = useState(0);
+
+  const goToPrev = () => {
+    setActivePageIndex((index) => index - 1);
+  };
+
+  const goToNext = () => {
+    setActivePageIndex((index) => index + 1);
+  };
+
+  const context = {
+    activePageIndex,
+    goToNext,
+    goToPrev,
+    steps,
+    setSteps
+  };
+
+  return (
+    <WizardContext.Provider value={context}>
+      <div className="wizard-container">{children}</div>
+    </WizardContext.Provider>
+  );
+}
+
+Wizard.Pages = Pages;
+Wizard.ButtonNext = ButtonNext;
+Wizard.ButtonPrev = ButtonPrev;
+
 export default function App() {
   return (
-    <Wizard>
-      <Page1 />
-      <Page2 />
-      <Page3 />
-    </Wizard>
+    <>
+      <Wizard>
+        <Wizard.Pages>
+          <Page1 />
+          <Page2 />
+          <Page3 />
+        </Wizard.Pages>
+        <div className="wizard-actions">
+          <Wizard.ButtonPrev type="button" className="wizard__buttons-left">
+            Previous
+          </Wizard.ButtonPrev>
+          <Wizard.ButtonNext type="button" className="wizard__buttons-left">
+            Next
+          </Wizard.ButtonNext>
+        </div>
+      </Wizard>
+    </>
   );
 }
